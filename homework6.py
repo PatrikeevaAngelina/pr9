8ff6a6272210af903b84730264eb1a7ab352ce9d
PatrikeevaAngelina <angelinapatrikeeva9@gmail.com>
2024-10-29 19:39:58 +0500

    homework6

---
+++ homework6.py
@@ -0,0 +1,8 @@
+import random
+numbers = random.sample(range(1, 20), 10) # Создаем случайный список чисел
+print("Исходный список:", numbers)
+min = numbers.index(min(numbers))
+max= numbers.index(max(numbers))
+numbers[min], numbers[max] = numbers[max], numbers[min]
+
+print(f"Список после перестановки: {numbers}")
